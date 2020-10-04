3367eafd5188e5ac8fc7ddb3a0060deec29d33cc
DJ Coleman <DJ@DJs-MacBook-Pro.attlocal.net>
2020-10-04 16:21:37 -0700

    finished iterative sorting

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,16 @@
 def linear_search(arr, target):
     # Your code here
 
-
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
+# Compare the item in the middle of the data set to the item we are searching for.
+# If it is the same, stop. We found it and are done!
+# Else, if the item we are searching for is LESS than the item in the middle:
+# Eliminate the RHS of list. Repeat step 1 with only the LHS of list.
+# Else, the item we are searching for is GREATER than the item in the middle:
+# Eliminate the LHS of list. Repeat step 1 with only the RHS of the list.
 def binary_search(arr, target):
-
-    # Your code here
-
 
     return -1  # not found
